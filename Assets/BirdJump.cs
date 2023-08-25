35de6583525111c9e2185d2b73b344f015009495
es5es5 <es5e5s@nate.com>
2023-08-25 17:55:40 +0900

    GameOverScene

--- Assets/BirdJump.cs
+++ Assets/BirdJump.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BirdJump : MonoBehaviour
 {
@@ -15,9 +16,14 @@ public class BirdJump : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) // 마우스 왼쪽 버튼
+        if (Input.GetMouseButtonDown(0))
         {
             rb.velocity = Vector2.up * jumpPower; // (0, 1)
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SceneManager.LoadScene("GameOverScene");
     }
 }
